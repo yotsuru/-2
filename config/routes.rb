d780519bb8d2c4d4006791c6bce6d5164ea1d886
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-06-22 10:49:13 +0000

    [Add]新規投稿から詳細画面への遷移

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
   
-  get 'lists/index'
-  get 'lists/show'
+  get 'lists' => 'lists#index'
+  # 'lists/:id'=>'lists#show'
   get 'lists/edit'
   get '/top'=>'homes#top'
+  get 'lists/:id' => 'lists#show', as: 'list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
