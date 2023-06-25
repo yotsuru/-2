f96c5bfc6788289db054b4111bcd9e17374cbfe0
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-06-25 12:36:00 +0000

    [Add]バリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,10 @@ class BooksController < ApplicationController
   end
   
   def create
-    book=Book.new(book_params)
-    if book.save
+    @book=Book.new(book_params)
+    if @book.save
       flash[:notice]="Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :new
     end
