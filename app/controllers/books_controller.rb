7734858930389c3eebff28d9614a735a8d54058d
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-06-25 13:05:07 +0000

    [Add]Editページへのバリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,10 +26,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice]="Book was successfully updated."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice]="Book was successfully updated."
+       redirect_to book_path(book.id)
+    else
+      render :'books/edit'
+    end
   end
   
   def destroy
