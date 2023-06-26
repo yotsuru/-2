5f9666688913c17f29727657eed73a7b63515727
yotsuru <135504464+yotsuru@users.noreply.github.com>
2023-06-26 11:36:27 +0000

    [Update]投稿フォームのバリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,8 @@ class BooksController < ApplicationController
       flash[:notice]="Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books=Book.all
+      render :index
     end
   end
 
